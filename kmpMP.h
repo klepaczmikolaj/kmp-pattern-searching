38d62e738d1161514ff544409dc37107473c36a6
klepaczmikolaj <klepaczmikolaj@gmail.com>
2019-12-15 18:02:47 +0100

    add cmd arguments

--- kmpMP.h
+++ kmpMP.h
@@ -1,8 +1,8 @@
 #ifndef KMPMP_H
 #define KMPMP_H
 
-int morrisPrattMP(char *text, char *pattern, int *patPositions);
-int knuthMorrisPrattMP(char *text, char *pattern, int *patPositions);
+int morrisPrattMP(char *text, char *pattern, int *patPositions, int thread_count);
+int knuthMorrisPrattMP(char *text, char *pattern, int *patPositions, int thread_count);
 
 
 #endif //KMPMP_H
